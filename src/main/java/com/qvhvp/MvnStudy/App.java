cd8549a63b2e2ba27a218d0a8f0b50f2bf468898
wangping <4545945@163.com>
2015-06-15 22:52:52 +0800

    cm

---
+++ src/main/java/com/qvhvp/MvnStudy/App.java
@@ -0,0 +1,13 @@
+package com.qvhvp.MvnStudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
